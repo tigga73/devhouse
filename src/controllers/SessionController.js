d83df89b4ee5755908db9a38a9007ae713ba8383
Tiago Silva Cardoso - 06453943514 <tiago.s.cardoso@animaeducacao.com.br>
2023-08-10 09:51:05 -0300

    adding required fields validation to email

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,8 +1,17 @@
 import User from '../models/User';
+import * as Yup from 'yup';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+    });
+
     const { email } = req.body;
+
+    if (!schema.isValid(req.body)) {
+      return res.status(400).json({ error: 'E-mail é um campo obeigatório' });
+    }
 
     let user = await User.findOne({ email });
     if (!user) {
